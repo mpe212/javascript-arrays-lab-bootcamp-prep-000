69f14593998402aa1a461fe19b5403eafbb5253c
Marcia Elyseu <marcia.pelyseu@gmail.com>
2018-06-03 20:30:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,52 +2,52 @@
 
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
-// function destructivelyAppendKitten(name){
-//   kittens.push(name); 
-//   return kittens;
-// }
+function destructivelyAppendKitten(name){
+  kittens.push(name); 
+  return kittens;
+}
 
-// console.log(destructivelyAppendKitten('Ralph'));
+console.log(destructivelyAppendKitten('Ralph'));
 
-// function destructivelyPrependKitten(name){
-//   kittens.unshift(name);
-//   return kittens;
-// }
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
 
-// console.log(destructivelyPrependKitten("Bob"));
+console.log(destructivelyPrependKitten("Bob"));
 
-// function destructivelyRemoveLastKitten(){
-//   kittens.pop();
-//   return kittens;
-// }
-// console.log(destructivelyRemoveLastKitten(kittens));
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+console.log(destructivelyRemoveLastKitten(kittens));
 
-// function destructivelyRemoveFirstKitten(){
-//   kittens.shift();
-//   return kittens;
-// }
-// console.log(destructivelyRemoveFirstKitten(kittens));
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+console.log(destructivelyRemoveFirstKitten(kittens));
 
-// function appendKitten(name){
-//   var newKittens = [...kittens, name];
-//   return newKittens;
-// }
+function appendKitten(name){
+  var newKittens = [...kittens, name];
+  return newKittens;
+}
 
-// console.log(appendKitten("Broom"));
+console.log(appendKitten("Broom"));
 
-// function prependKitten(name){
-//   var newKittens = [name, ...kittens];
-//   return newKittens;
-// }
+function prependKitten(name){
+  var newKittens = [name, ...kittens];
+  return newKittens;
+}
 
-// console.log(prependKitten("Arnold"));
+console.log(prependKitten("Arnold"));
 
-// function removeLastKitten(){
-//   var newKittens = kittens.slice(0,kittens.length-1);
-//   console.log(newKittens);
-//   return newKittens;
-// }
-// removeLastKitten();
+function removeLastKitten(){
+  var newKittens = kittens.slice(0,kittens.length-1);
+  console.log(newKittens);
+  return newKittens;
+}
+removeLastKitten();
 
 
 function removeFirstKitten(){
