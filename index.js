89f620da3eefb6cc143c1eca679f813b69d3e188
Marcia Elyseu <marcia.pelyseu@gmail.com>
2018-06-03 20:00:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 destructivelyAppendKitten(name){
-  
+ kittens.push(name); 
+ return kittens;
 }
 
 destructivelyAppendKitten('Ralph');
